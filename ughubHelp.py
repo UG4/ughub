9eb9cf8d5285f07cba4a3156094b0f094120910f
stephanmg <stephan@syntaktischer-zucker.de>
2016-03-20 18:06:48 +0100

    Spaces and Tabs in indenting mixed. PEP 8 recommends spaces for indenting, also Python 3 disallows files with spaces and tabs mixed in indenting.

--- ughubHelp.py
+++ ughubHelp.py
@@ -29,76 +29,80 @@
 import ughubUtil
 import ughubHelpContents
 
-class MalformedHelpContentsError(Exception) : pass
+
+class MalformedHelpContentsError(Exception): pass
+
 
 def GetHelpEntry(entry):
-	return ughubUtil.GetFromNestedTable(ughubHelpContents.content, entry)
+    return ughubUtil.GetFromNestedTable(ughubHelpContents.content, entry)
 
 
 def GetCommandsInHelp():
-	try:
-		d = GetHelpEntry("commands")
-		out = []
+    try:
+        d = GetHelpEntry("commands")
+        out = []
 
-		if type(d) == list:
-			for e in d:
-				name = e["name"]
-				if type(name) == str:
-					out.append(name)
-				elif type(name) == list and len(name) > 0:
-					s = name[0]
-					for i in range(1, len(name)):
-						s = s + ", " + name[i]
-					out.append(s)
-		else:
-			raise MalformedHelpContentsError("'commands' entry has to be a list")
-	except ughubUtil.NestedTableEntryNotFoundError as e:
-		raise MalformedHelpContentsError("'commands' list required in help contents")
+        if type(d) == list:
+            for e in d:
+                name = e["name"]
+                if type(name) == str:
+                    out.append(name)
+                elif type(name) == list and len(name) > 0:
+                    s = name[0]
+                    for i in range(1, len(name)):
+                        s = s + ", " + name[i]
+                    out.append(s)
+        else:
+            raise MalformedHelpContentsError("'commands' entry has to be a list")
+    except ughubUtil.NestedTableEntryNotFoundError as e:
+        raise MalformedHelpContentsError("'commands' list required in help contents")
 
-	return out
+    return out
 
 
 def PrintUsage():
-	try:
-		print(GetHelpEntry("usage"))
-	except ughubUtil.NestedTableEntryNotFoundError as e:
-		raise MalformedHelpContentsError(e)
+    try:
+        print(GetHelpEntry("usage"))
+    except ughubUtil.NestedTableEntryNotFoundError as e:
+        raise MalformedHelpContentsError(e)
 
 
 def PrintCommands():
-	for cmd in GetCommandsInHelp():
-		print("  {0}".format(cmd))
+    for cmd in GetCommandsInHelp():
+        print("  {0}".format(cmd))
+
 
 # Prints help for the command specified in 'cmd'.
 def PrintCommandHelp(cmdName):
-	try:
-		cmdDict = GetHelpEntry("commands.{0}".format(cmdName))
-		print("Usage: ughub {0}".format(cmdDict["usage"]))
-		print("")
-		for line in cmdDict["description"].splitlines():
-			print("  {0}".format(line))
+    try:
+        cmdDict = GetHelpEntry("commands.{0}".format(cmdName))
+        print("Usage: ughub {0}".format(cmdDict["usage"]))
+        print("")
+        for line in cmdDict["description"].splitlines():
+            print("  {0}".format(line))
 
-		try:
-			options = ughubUtil.GetFromNestedTable(cmdDict, "options")
-			print("")
-			print("Valid options:")
-			for opt in options:
-				name = opt["name"]
-				sep = ":"
-				for line in opt["description"].splitlines():
-					print("  {0:20}{1} {2}".format(name, sep, line))
-					name = ""
-					sep = " "
-		except ughubUtil.NestedTableEntryNotFoundError:
-			pass
+        try:
+            options = ughubUtil.GetFromNestedTable(cmdDict, "options")
+            print("")
+            print("Valid options:")
+            for opt in options:
+                name = opt["name"]
+                sep = ":"
+                for line in opt["description"].splitlines():
+                    print("  {0:20}{1} {2}".format(name, sep, line))
+                    name = ""
+                    sep = " "
+        except ughubUtil.NestedTableEntryNotFoundError:
+            pass
 
-	except ughubUtil.NestedTableEntryNotFoundError:
-		raise MalformedHelpContentsError("Requested command '{0}' not found in help database"
-			  							 .format(cmdName))
+    except ughubUtil.NestedTableEntryNotFoundError:
+        raise MalformedHelpContentsError("Requested command '{0}' not found in help database"
+                                         .format(cmdName))
+
 
 # Prints help on how to use the help command and a list of all available commands
 def PrintHelp():
-	PrintCommandHelp("help")
-	print("")
-	print("available commands:")
-	PrintCommands()
+    PrintCommandHelp("help")
+    print("")
+    print("available commands:")
+    PrintCommands()
