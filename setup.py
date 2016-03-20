eef4f6284409ea0e7f6507c4cb444f0d0de980e6
Martin Scherer <m.scherer@fu-berlin.de>
2016-03-20 04:34:51 +0100

    added some info

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ metadata = dict(name="ughub",
                        "plugins and applications that are built on top of UG4."
                        " It automatically handles inter-package dependencies "
                        "and helps in managing the different involved git repositories.",
+      url='https://github.com/UG4/ughub/',
+      author='Sebastian Reiter',
+      author_email='sreiter@gcsc.uni-frankfurt.de',
+      license="3-Clause BSD",
       package_dir={'ughub': '.'},
       packages=['ughub'],
       entry_points={"console_scripts": ["ughub = ughub.ughub:main"] },
